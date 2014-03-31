fd60240592dab759e77d281bdb48389cb8ef6567
WebMaestroFr <etienne@webmaestro.fr>
2014-03-31 14:04:40 +0900

    Fixed initial container height

--- js/wm-parallax.js
+++ js/wm-parallax.js
@@ -8,18 +8,22 @@
       setOrigin = function () {
         api.hw = api.ox + (api.ow / 2);
         api.hh = api.oy + (api.oh / 2);
+      },
+      minHeight = function (h) {
+        if (h > scene.height()) { scene.height(h); }
       };
-    children.css('opacity', 0).each(function () {
+    scene.height(0);
+    children.hide().each(function () {
       var id = $(this).data('layer-id');
+      minHeight(layers[id].height);
       $('<img>').attr(layers[id]).appendTo(this).load(function () {
         loaded += 1;
         if (loaded === children.length) {
+          scene.css({ backgroundImage: 'none' });
+          children.fadeIn();
           api = $(scene).parallax().data('api');
           api.onWindowResize = setOrigin;
           setOrigin();
-          children.animate({ opacity: 1 }, function () {
-            scene.css({ backgroundImage: 'none' });
-          });
         }
       });
     });
